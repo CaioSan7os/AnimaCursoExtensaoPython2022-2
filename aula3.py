17a4db1b201acf95089b35600e85c3d1bdebbdc7
Caio Santos <78771496+CaioSan7os@users.noreply.github.com>
2022-11-09 23:24:06 +0000

    exemplos de estrutura While, aula 3

---
+++ aula3.py
@@ -0,0 +1,19 @@
+#contador = 1
+#while(contador <= 10):
+ # print(contador)
+  #contador = contador + 1
+
+frutas = ["banana", "pêra", "Abacaxi"]
+#print(frutas[2], len(frutas))
+frutas.append("manga")
+
+print(frutas[0])
+print(frutas[1])
+print(frutas[2])
+print(frutas[3])
+#print(frutas[4])
+print("estrutura While")
+i=0
+while(i<len(frutas)):
+  print(frutas[i])
+  i = i + 1
